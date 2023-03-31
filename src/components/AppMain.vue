93b283b748fe452fa5f3dd118deee9eab608045f
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-03-31 18:18:15 +0200

    Milestone 2 ended - flags added

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,8 @@ import { store } from '../store';
 
 export default {
     emits: ['submit'], //submit event
-    setup() { //Merge results of movies and tv series
+    setup() {
+
         const searchResults = computed(() => {
             const movies = store.movies.map(movie => {
                 return {
@@ -14,6 +15,7 @@ export default {
                     original_title: movie.original_title
                 }
             });
+
             const tv = store.tv.map(tvShow => {
                 return {
                     ...tvShow,
@@ -22,10 +24,24 @@ export default {
                 }
             });
             return [...movies, ...tv];
-        });
+        });//Merge results of movies and tv series
+
+        const languages = {
+            it: 'it',
+            en: 'gb',
+            fr: 'fr',
+            pt: 'pt',
+            ja: 'jp',
+            cn: "cn",
+            ru: "ru",
+            es: "es",
+            ko: "kr"
+        } //Language supported ticker and modified ticker to work with flag-icon-css
+
         return {
             store,
-            searchResults
+            searchResults,
+            languages
         };
     }
 }
@@ -38,9 +54,17 @@ export default {
     <div v-for="result in searchResults">
         <h1>{{ result.title || result.name }}</h1> <!-- Search for a key, if it's not avaible search for a second one -->
         <p>{{ result.original_title || result.original_name }}</p>
+        <i v-if="languages[result.original_language]"
+            :class="['flag-icon', 'flag-icon-' + languages[result.original_language]]"></i>
+        <i v-else class="fa-solid fa-globe"></i>
         <p>{{ result.original_language }}</p>
         <p>{{ result.vote_average }}</p>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flag-icon {
+    height: 25px;
+    width: 25px;
+}
+</style>
